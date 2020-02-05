34c842d3ae9509fb5b067bb173790346a2aaa5fd
kobutaMyu <sandpig2012@gmail.com>
2020-02-05 20:50:39 +0900

    Create Project

---
+++ Assets/Script/WorldMapFunction/MapArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EIJ.WorldMap
+{
+    public class MapArea : MonoBehaviour
+    {
+        [SerializeField] MapAreaType m_MapAreaType = null;
+        bool m_IsVisible = false;
+        bool m_IsFogged = true;
+        public MapAreaType MapAreaType { get { return m_MapAreaType; } }
+        public bool IsVisible { get { return m_IsVisible; } set { m_IsVisible = value;  } }
+        public bool IsFogged { get { return m_IsFogged; } set { m_IsFogged = value; } }
+    }
+}
---
+++ Assets/Script/WorldMapFunction/WorldMapManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace EIJ.WorldMap
+{
+    public class WorldMapManager : MonoBehaviour
+    {
+        [Serializable]
+        internal class AreaInfo
+        {
+            [SerializeField] private MapArea m_MapArea;
+            public MapArea MapArea { get { return m_MapArea; } }
+
+
+            public AreaInfo(MapArea mapArea)
+            {
+                m_MapArea = mapArea;
+            }
+        }
+    }
+}
