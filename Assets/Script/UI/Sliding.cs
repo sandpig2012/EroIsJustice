bbccd09d150f5e3ae2e43984ac389a61b327c68c
AtelierRozen <atelierrozen@hotmail.com>
2020-02-29 23:06:27 +0900

    UI测试

---
+++ Assets/Script/UI/Sliding.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliding : MonoBehaviour
+{
+    public GameObject skill;
+    Transform slidintransform;
+    Transform slidouttransform;
+    Vector3 pos;
+    public float distance;
+    Vector3 diriction = new Vector3(1, 0, 0);
+
+
+    public void slidin(){
+
+        distance = 0.0f;
+
+        while (distance < 200.0f)
+        {
+            slidintransform = skill.GetComponent<Transform>();
+            slidintransform.position += diriction;
+            distance += 1.0f;
+        }
+    }
+
+    public void slidout(){
+        distance = 200.0f;
+        while (distance > 0.0f)
+        {
+            slidintransform = skill.GetComponent<Transform>();
+            slidintransform.position -= diriction;
+            distance -= 1.0f;
+        }
+    }
+
+
+
+}
---
+++ Assets/Script/UI/sceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void changeScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
