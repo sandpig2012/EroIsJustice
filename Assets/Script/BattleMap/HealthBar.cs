c949e708fd9dde9c82039900c613d2d01fed4d7b
AtelierRozen <atelierrozen@hotmail.com>
2020-06-10 22:39:42 +0900

    数据更新

---
+++ Assets/Script/BattleMap/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider Slider;
+
+    public void SetMaxhealth(int maxHealth) 
+    {
+        Slider.maxValue = maxHealth;
+        Slider.value = maxHealth;
+    }
+
+    public void SetCurrentHealth(int currentHealth) 
+    {
+        Slider.value = currentHealth;
+    }
+}
